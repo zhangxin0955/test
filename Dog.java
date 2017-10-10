36eb2dab4cc96a8c7fefad2a25bb0eca2b7a1bca
zhangxin <zhangxinjob@yeah.net>
2017-10-10 15:49:52 +0800

    1删除

---
+++ Dog.java
@@ -0,0 +1,18 @@
+package com.hpeu.dogbark;
+
+public class Dog {
+	public void bark() {
+		System.out.println("狗叫...");
+		System.out.println("狗叫...");
+	}
+
+	public void brak(int times) {
+		
+
+		
+
+
+
+System.out.println("狗叫：" + times + "声！");
+	}
+}
